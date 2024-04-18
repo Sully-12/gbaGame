824bd945754e9e5f2dbe2cc9986017bd0876a264
Sullivan Smith <ssmith46@mail.umw.edu>
2024-04-18 17:41:42 -0400

    More comments

--- calcXscroll.s
+++ calcXscroll.s
@@ -1,19 +1,25 @@
 @calcXscroll.s
 
 .global calcXscroll
-
+@This function calculates how fast the screen should be scrolling
+@Based on how far into the game the user has made it
 calcXscroll:
-    @given just the input of xscroll
+    @r0 = xscroll distance
+    @Check if made it around one time
     cmp r0, #256
     blt .first
+    @Check second time
     cmp r0, #768
     blt .second
+    @Third time
     cmp r0, #1280
     blt .third
     
+    @4+ times by this point
     mov r0, #4
     b .end
 
+    @Give each one its necessary scroll speed
     .first:
         mov r0, #1
         b .end
@@ -24,5 +30,6 @@ calcXscroll:
         mov r0, #3
         b .end
 
+    @Return the speed to scroll
     .end:
         mov pc, lr
--- textSetup.s
+++ textSetup.s
@@ -2,7 +2,9 @@
 
 .global textSetup
 
+@This function will reset the data in the text screen to be zero
 textSetup:
+    @Store the data in importnat memory spots first
     .storeStuff: 
         sub sp, sp, #4
         str lr, [sp]
@@ -13,19 +15,30 @@ textSetup:
         sub sp, sp, #4
         str r5, [sp]    
 
+    @Set the variables default values
     .initializeStuff:
+        @32x32=1024
         mov r1, #1024
+        @Need to replace with 0
         mov r2, #0
+        @The start of the array
         mov r4, r0
+        @Where at in the array
         mov r5, #0
-
+    
+    @Continue iterating until all pixels have been cleared
     .iterate:
+        @See if end condition has been met
         cmp r5, r1
         beq .done
+        @Store a zero at this position
         strb r2, [r4, r5]
+        @Increment the counter
         add r5, r5, #1
+        @Go back to the top
         b .iterate
 
+    @Reload the saved data and return
     .done:
         ldr r5, [sp]
         add sp, sp, #4
